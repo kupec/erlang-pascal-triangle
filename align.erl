f14fe83599a4ed88715b6f84ea3bee5ea5684374
kupec <kupec-k@ya.ru>
2016-03-12 12:46:32 +0300

    Add justify strings

--- align.erl
+++ align.erl
@@ -1,8 +1,10 @@
 -module(align).
 -export([
-    buildLine/2
+    buildLine/2,
+    justify/2
 ]).
 
+% export
 buildLine(Strings, Wholes) ->
     Spaces = stringifyWholes(Wholes),
     AlignedStrings = mixLists(Spaces, Strings),
@@ -32,4 +34,30 @@ mixLists(Result, [], Second) -> Result ++ Second;
 mixLists(Result, [F|FRest], [S|SRest]) ->
     NextResult = [F, S] ++ Result,
     mixLists(NextResult, FRest, SRest).
+
+% export
+justify(Length, Strings) ->
+    SpaceCount = getSpaceCount(Length, Strings),
+    NumWholes = length(Strings) + 1,
+    Wholes = splitNumToList(SpaceCount, NumWholes),
+    buildLine(Strings, Wholes).
+
+getSpaceCount(Length, Strings) ->
+    TotalChars = lists:concat(Strings),
+    max(0, Length - length(TotalChars)).
+
+splitNumToList(Number, Length) ->
+    Common = Number div Length,
+    Shares = Number rem Length,
+    formSplitList(Common, Shares, Length).
+
+formSplitList(Common, Shares, Length) -> formSplitList([], Common, Shares, Length).
+
+formSplitList(Result, _, _, 0) -> Result;
+formSplitList(Result, Common, 0, Length) ->
+    NextResult = Result ++ [Common],
+    formSplitList(NextResult, Common, 0, Length - 1);
+formSplitList(Result, Common, Shares, Length) ->
+    NextResult = Result ++ [Common + 1],
+    formSplitList(NextResult, Common, Shares - 1, Length - 1).
 
