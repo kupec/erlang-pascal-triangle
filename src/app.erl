6f5960aa58aea5c98d4cf508f0de5a43e1d15bf5
kupec <kupec-k@ya.ru>
2016-03-13 16:44:07 +0300

    Remove dependecies to triangleLine from app module

--- src/app.erl
+++ src/app.erl
@@ -8,30 +8,36 @@ start() ->
     io:fwrite("Version ~s~n", [?VERSION]),
 
     {ok, [N]} = io:fread("Type a number: ", "~d"),
-    W = findOptimalTriangleWidth(N),
-    Triangle = formPascalTriangle(W, N),
+    Triangle = pascal:triangle(N),
 
-    io:fwrite("~s", [Triangle]).
+    TriangleFormat = formPascalTriangle(Triangle),
 
-findOptimalTriangleWidth(0) -> 1;
-findOptimalTriangleWidth(N) ->
-    LastLineLength = charLengthTriangleLine(N),
-    PreLastLineLength = charLengthTriangleLine(N - 1),
+    io:fwrite("~s", [TriangleFormat]).
+
+
+formPascalTriangle(Triangle) ->
+    W = findOptimalTriangleWidth(Triangle),
+    TriangleFormat = formPascalTriangle([], Triangle, W, collapse),
+    lists:concat(TriangleFormat).
+
+
+findOptimalTriangleWidth([ [1] ]) -> 1;
+findOptimalTriangleWidth(Triangle) ->
+    N = length(Triangle) - 1,
+    LastLineLength = triangleLineCharsLength(Triangle, N),
+    PreLastLineLength = triangleLineCharsLength(Triangle, N - 1),
     LastLineLength + PreLastLineLength.
 
-charLengthTriangleLine(N) ->
-    Line = pascal:triangleLine(N),
+triangleLineCharsLength(Triangle, LineIndex) ->
+    Line = lists:nth(LineIndex + 1, Triangle),
     String = lists:concat(Line),
     length(String).
 
-formPascalTriangle(W, N) ->
-    Triangle = formPascalTriangle([], W, N, collapse),
-    lists:concat(Triangle).
-
-formPascalTriangle(Result, _, -1, _) -> Result;
-formPascalTriangle(Result, W, N, JustifyOption) ->
-    PascalLine = pascal:triangleLine(N),
+formPascalTriangle(Result, [], _, _) -> Result;
+formPascalTriangle(Result, Triangle, W, JustifyOption) ->
+    PascalLine = lists:last(Triangle),
     Line = align:justify(W, PascalLine, JustifyOption),
     NextResult = [Line, "\n"] ++ Result,
-    formPascalTriangle(NextResult, W, N - 1, simple).
+    TruncTriangle = lists:delete(PascalLine, Triangle),
+    formPascalTriangle(NextResult, TruncTriangle, W, simple).
 
