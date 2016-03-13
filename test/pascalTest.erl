b991deaf34b2e1e45ae874bc4e8db8234c65fe97
kupec <kupec-k@ya.ru>
2016-03-13 15:52:32 +0300

    Add triangle method for pascal

--- test/pascalTest.erl
+++ test/pascalTest.erl
@@ -10,3 +10,12 @@ triangleLine_test_() ->
     ?_assertEqual(F(5), [1, 5, 10, 10, 5, 1])
     ].
 
+triangle_test_() ->
+    F = fun pascal:triangle/1,
+    [
+    ?_assertEqual(F(0), [ [1] ]),
+    ?_assertEqual(F(1), [ [1], [1, 1] ]),
+    ?_assertEqual(F(2), [ [1], [1, 1], [1, 2, 1] ]),
+    ?_assertEqual(F(4), [ [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1] ])
+    ].
+
