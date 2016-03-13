ad9d27ad00a73da95d988e340feb256555e1eaa4
kupec <kupec-k@ya.ru>
2016-03-13 15:36:01 +0300

    Refactor unitest

--- test/pascalTest.erl
+++ test/pascalTest.erl
@@ -2,9 +2,11 @@
 -include_lib("eunit/include/eunit.hrl").
 
 triangleLine_test_() ->
+    F = fun pascal:triangleLine/1,
     [
-    ?_assertEqual(pascal:triangleLine(0), [1]),
-    ?_assertEqual(pascal:triangleLine(1), [1, 1]),
-    ?_assertEqual(pascal:triangleLine(2), [1, 2, 1]),
-    ?_assertEqual(pascal:triangleLine(5), [1, 5, 10, 10, 5, 1])
+    ?_assertEqual(F(0), [1]),
+    ?_assertEqual(F(1), [1, 1]),
+    ?_assertEqual(F(2), [1, 2, 1]),
+    ?_assertEqual(F(5), [1, 5, 10, 10, 5, 1])
     ].
+
