31fa4dd30ee0ed2432761589669a59912240c8fe
kupec <kupec-k@ya.ru>
2016-03-14 20:24:12 +0300

    Clean formatter test

--- test/pascalFormatterTest.erl
+++ test/pascalFormatterTest.erl
@@ -5,21 +5,18 @@ format_test_() ->
     F = fun(N) -> pascalFormatter:format(pascal:triangle(N)) end,
     [
     ?_assertEqual(F(0),
-"1
-"
+"1\n"
 ),
     ?_assertEqual(F(1),
-" 1 
-1 1
-"
+" 1 \n"
+"1 1\n"
 ),
     ?_assertEqual(F(4),
-"    1    
-   1  1  
-  1  2 1 
- 1 3 3 1 
-1 4 6 4 1
-"
+"    1    \n"
+"   1  1  \n"
+"  1  2 1 \n"
+" 1 3 3 1 \n"
+"1 4 6 4 1\n"
 )
     ].
 
